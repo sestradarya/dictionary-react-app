bb8992279ca3d56fa18a7e8fccc7293c9f44daee
sestradarya <nguendarina1@gmail.com>
2023-05-14 21:45:30 +0300

    search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { useState } from 'react'
+// import {useNavigate } from 'react-router-dom'
+import styled from 'styled-components'
 
 const Search = () => {
+
+    const [input, setInput] = useState('')
+    // const navigate = useNavigate()
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        // navigate('searched/' + input)
+    }
+
   return (
-    <div>
-      
-    </div>
+    <Form onSubmit={submitHandler()}>
+      <input type="text" value={input} onChange={(e) => {setInput(e.target.value)}}/>
+    </Form>
   )
 }
+
+const Form = styled.form`
+     margin: 1rem 20rem 2rem;
+  position: relative;
+
+  input {
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.2rem;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 2rem;
+    outline: none;
+    width: 100%;
+  }
+  svg {
+    position: absolute;
+    top: 50%;
+    left: -1%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`
+
+
 
 export default Search
