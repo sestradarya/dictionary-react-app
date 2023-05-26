e670e0f33953bdcbce99aef2f6ad2d5198c81020
sestradarya <nguendarina1@gmail.com>
2023-05-26 14:49:12 +0300

    input stylization

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,48 +1,98 @@
-import React from 'react'
-import { useState } from 'react'
-import {useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-const Search = ({startSearch}) => {
+const Search = ({ startSearch }) => {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState('')
-
-    const submitHandler = (event) => {
-        event.preventDefault();
-        startSearch(input)
-    }
+  const submitHandler = (event) => {
+    event.preventDefault();
+    startSearch(input);
+  };
 
   return (
     <Form onSubmit={submitHandler}>
-      <input type="text" value={input} onChange={(e) => {setInput(e.target.value)}}/>
+      <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
+        <g>
+          <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+        </g>
+      </svg>
+      <input
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+        placeholder="Search"
+        type="search"
+        class="input"
+        value={input}
+      />
     </Form>
-  )
-}
+  );
+};
 
 const Form = styled.form`
-    margin: 1rem 20rem 2rem;
-    position: relative;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  line-height: 28px;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  position: relative;
+  max-width: 190px;
 
-  input {
-    border: none;
-    background: linear-gradient(35deg, #494949, #313131);
-    font-size: 1.2rem;
-    color: white;
-    padding: 1rem 2rem;
-    border: none;
-    border-radius: 2rem;
-    outline: none;
+  .input {
     width: 100%;
+    height: 40px;
+    line-height: 28px;
+    padding: 0 1rem;
+    padding-left: 2.5rem;
+    border: 2px solid transparent;
+    border-radius: 8px;
+    outline: none;
+    background-color: #f3f3f4;
+    color: #0d0c22;
+    -webkit-transition: 0.3s ease;
+    transition: 0.3s ease;
   }
-  svg {
+
+  .input::-webkit-input-placeholder {
+    color: #9e9ea7;
+  }
+
+  .input::-moz-placeholder {
+    color: #9e9ea7;
+  }
+
+  .input:-ms-input-placeholder {
+    color: #9e9ea7;
+  }
+
+  .input::-ms-input-placeholder {
+    color: #9e9ea7;
+  }
+
+  .input::placeholder {
+    color: #9e9ea7;
+  }
+
+  .input:focus,
+  input:hover {
+    outline: none;
+    border-color: #1a6dea;
+    background-color: #fff;
+    -webkit-box-shadow: 0 0 0 4px #1a6dea1c;
+    box-shadow: 0 0 0 4px #1a6dea1c;
+  }
+
+  .icon {
     position: absolute;
-    top: 50%;
-    left: -1%;
-    transform: translate(100%, -50%);
-    color: white;
+    left: 1rem;
+    fill: #1a6dea;
+    width: 1rem;
+    height: 1rem;
   }
-`
+`;
 
-
-
-export default Search
+export default Search;
